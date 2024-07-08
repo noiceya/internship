f80c9a1b969d32a6a5636bd161cf5b21c3abd42d
noiceya <91201243+noiceya@users.noreply.github.com>
2024-07-08 10:19:21 +0200

    Movie Details Page Updated

--- 0507/react_onePageSite/src/pages/MovieDetail/MovieDetail.jsx
+++ 0507/react_onePageSite/src/pages/MovieDetail/MovieDetail.jsx
@@ -1,19 +1,37 @@
+import React, { useContext, useEffect } from "react";
+import "./MovieDetail.css";
+import { myContext } from "../../app/context";
 
-import { useContext, useEffect } from "react"
-import "./MovieDetail.css"
-import { myContext } from "../../app/context"
+function MovieDetail() {
+  const { state } = useContext(myContext);
 
-function MovieDetail () {
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
-    const { state, SetAuth } = useContext(myContext)
+  // Check if state.auth.movie exists to prevent errors
+  if (!state.auth.movie) {
+    return <div>Loading...</div>;
+  }
 
-    useEffect(()=>{
-        console.log(state)
-    }, [state])
+  const { title, original_title, release_date, vote_average, vote_count, poster_path } = state.auth.movie;
 
-    return(
-        <div className="detail-design">{state.auth.movie?.title}</div>
-    )
+  return (
+    <div className="detail-design">
+      <div className="info">
+        <h1>{title}</h1>
+        <p><b>Release date</b><br></br>{release_date}</p>
+        <p><b>Original title</b><br></br>{original_title}</p>
+        <p><b>Average note</b><br></br>{vote_average}</p>
+        <p id="votes"><b>Votes</b><br></br>{vote_count}</p>
+      </div>
+      <img id="main"
+        src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+        alt={title}
+        className="movie-poster"
+      />
+    </div>
+  );
 }
 
-export default MovieDetail
+export default MovieDetail;
